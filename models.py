731719f5fcf1e7d50cb209ab3cb7f2e467329ed4
sdrobert <sdrobert@cs.toronto.edu>
2023-02-06 17:50:51 -0500

    Gogogo

---
+++ models.py
@@ -0,0 +1,301 @@
+# Copyright 2023 Sean Robertson
+#
+# Much of this code is based on that of github.com/facebookresearch/cpc_audio, which is
+# MIT-licensed. See LICENSE_cpc_audio for license details.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import abc
+from typing import Optional, Tuple, Type
+
+import torch
+import pydrobert.torch.modules as my_mods
+
+try:
+    import pytest
+
+    _parametrize = pytest.mark.parametrize
+except ImportError:
+    _parametrize = lambda x: x
+
+
+@torch.no_grad()
+def get_length_mask(
+    x: torch.Tensor, lens: torch.Tensor, batch_dim: int = 0, seq_dim: int = -1
+) -> torch.Tensor:
+    N, T = x.size(batch_dim), x.size(seq_dim)
+    assert lens.shape == (N,)
+    arange_shape, lens_shape = [1] * x.ndim, [1] * x.ndim
+    arange_shape[seq_dim] = T
+    lens_shape[batch_dim] = N
+    arange = torch.arange(T, device=x.device).view(arange_shape)
+    lens = lens.view(lens_shape)
+    return (lens > arange).expand_as(x)
+
+
+def check_positive(name: str, val, nonnegative=False):
+    pos = "non-negative" if nonnegative else "positive"
+    if val < 0 or (val == 0 and not nonnegative):
+        raise ValueError(f"Expected {name} to be {pos}; got {val}")
+
+
+class MaskingLayer(torch.nn.Module):
+
+    __slots__ = ["window", "padding", "stride", "batch_dim", "seq_dim"]
+    window: int
+    stride: int
+    padding: int
+    batch_dim: int
+    seq_dim: int
+
+    def __init__(
+        self,
+        window: int = 1,
+        stride: int = 1,
+        padding: int = 0,
+        batch_dim: int = 0,
+        seq_dim: int = -1,
+    ) -> None:
+        check_positive("window", window)
+        check_positive("stride", stride)
+        check_positive("padding", padding, True)
+        super().__init__()
+        self.window = window
+        self.stride = stride
+        self.padding = padding
+        self.batch_dim = batch_dim
+        self.seq_dim = seq_dim
+
+    def forward(
+        self, x: torch.Tensor, lens: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        if lens is None:
+            return x, lens
+        lens = (lens + 2 * self.padding - self.window) // self.stride + 1
+        return (
+            x.masked_fill(~get_length_mask(x, lens, self.batch_dim, self.seq_dim), 0.0),
+            lens,
+        )
+
+
+class ChannelNorm(torch.nn.Module):
+
+    __slots__ = ["epsilon"]
+
+    epsilon: float
+
+    def __init__(self, num_features: int, epsilon: float = 1e-05, affine: bool = True):
+        check_positive("num_features", num_features)
+        check_positive("epsilon", epsilon)
+        super(ChannelNorm, self).__init__()
+        if affine:
+            self.weight = torch.nn.parameter.Parameter(torch.empty(1, num_features, 1))
+            self.bias = torch.nn.parameter.Parameter(torch.empty(1, num_features, 1))
+        else:
+            self.register_parameter("weight", None)
+            self.register_parameter("bias", None)
+        self.epsilon = epsilon
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        if self.weight is not None:
+            torch.nn.init.ones_(self.weight)
+            torch.nn.init.zeros_(self.bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = x.mean(dim=1, keepdim=True)
+        var = x.var(dim=1, keepdim=True)
+        x = (x - mean) * torch.rsqrt(var + self.epsilon)
+        if self.weight is not None:
+            x = x * self.weight + self.bias
+        return x
+
+
+class Encoder(torch.nn.Module, metaclass=abc.ABCMeta):
+
+    __slots__ = ["in_channels", "hidden_size"]
+    in_channels: int
+    hidden_size: int
+
+    def __init__(self, in_channels: int, hidden_size: Optional[int] = None) -> None:
+        check_positive("in_channels", in_channels)
+        if hidden_size is None:
+            hidden_size = in_channels
+        else:
+            check_positive("hidden_size", hidden_size)
+        super().__init__()
+        self.in_channels, self.hidden_size = in_channels, hidden_size
+
+    @abc.abstractproperty
+    def downsampling_factor(self) -> int:
+        ...
+
+    @abc.abstractmethod
+    def encode(
+        self, x: torch.Tensor, lens: Optional[torch.Tensor]
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        ...
+
+    def forward(
+        self, x: torch.Tensor, lens: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        x, lens = self.encode(x, lens)
+        if lens is not None:
+            x = x.masked_fill(~get_length_mask(x, lens, seq_dim=1), 0.0)
+        return x, lens
+
+
+class IdentityEncoder(Encoder):
+    @property
+    def downsampling_factor(self) -> int:
+        return 1
+
+    def encode(
+        self, x: torch.Tensor, lens: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        return x, lens
+
+
+class ConvEncoder(Encoder):
+    def __init__(self, in_channels: int, hidden_size: int = 256) -> None:
+        super().__init__(in_channels, hidden_size)
+        self.mask0 = MaskingLayer(1, 1, 0)
+        self.conv1 = torch.nn.Conv1d(in_channels, hidden_size, 10, 5, 3)
+        self.norm1 = ChannelNorm(hidden_size)
+        self.mask1 = MaskingLayer(10, 5, 3)
+        self.conv2 = torch.nn.Conv1d(hidden_size, hidden_size, 8, 4, 2)
+        self.norm2 = ChannelNorm(hidden_size)
+        self.mask2 = MaskingLayer(8, 4, 2)
+        self.conv3 = torch.nn.Conv1d(hidden_size, hidden_size, 4, 2, 1)
+        self.norm3 = ChannelNorm(hidden_size)
+        self.mask3 = MaskingLayer(4, 2, 1)
+        self.conv4 = torch.nn.Conv1d(hidden_size, hidden_size, 4, 2, 1)
+        self.norm4 = ChannelNorm(hidden_size)
+        self.conv5 = torch.nn.Conv1d(hidden_size, hidden_size, 4, 2, 1)
+        self.norm5 = ChannelNorm(hidden_size)
+
+    def reset_parameters(self) -> None:
+        self.conv1.reset_parameters()
+        self.norm1.reset_parameters()
+        self.conv2.reset_parameters()
+        self.norm2.reset_parameters()
+        self.conv3.reset_parameters()
+        self.norm3.reset_parameters()
+        self.conv4.reset_parameters()
+        self.norm4.reset_parameters()
+        self.conv5.reset_parameters()
+        self.norm5.reset_parameters()
+
+    @property
+    def downsampling_factor(self) -> int:
+        return 160
+
+    def encode(
+        self, x: torch.Tensor, lens: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        x = x.transpose(1, 2)  # N, C, T
+        x, lens = self.mask0(x, lens)
+        x, lens = self.mask1(torch.nn.functional.relu(self.norm1(self.conv1(x))), lens)
+        x, lens = self.mask2(torch.nn.functional.relu(self.norm2(self.conv2(x))), lens)
+        x, lens = self.mask3(torch.nn.functional.relu(self.norm3(self.conv3(x))), lens)
+        x, lens = self.mask3(torch.nn.functional.relu(self.norm4(self.conv4(x))), lens)
+        x, lens = self.mask3(torch.nn.functional.relu(self.norm5(self.conv5(x))), lens)
+        x = x.transpose(1, 2)  # N, T, C
+        return x, lens
+
+
+class TransformerEncoder(Encoder):
+
+    __slots__ = ["in_channels", "hidden_size", "num_heads"]
+    num_heads: int
+
+    def __init__(
+        self,
+        in_channels: int,
+        num_layers: int = 1,
+        num_heads: int = 8,
+        dim_feedforward: int = 2048,
+        layer_norm_eps: float = 1e-5,
+        enable_nested_tensor: bool = False,
+    ) -> None:
+        check_positive("num_layers", num_layers)
+        check_positive("num_heads", num_heads)
+        check_positive("dim_feedforward", dim_feedforward)
+        check_positive("layer_norm_eps", layer_norm_eps)
+        super().__init__(in_channels)
+        self.num_heads = num_heads
+        encoder_layer = torch.nn.TransformerEncoderLayer(
+            in_channels,
+            num_heads,
+            dim_feedforward,
+            layer_norm_eps=layer_norm_eps,
+            batch_first=True,
+        )
+        layer_norm = torch.nn.LayerNorm(in_channels, layer_norm_eps)
+        self.encoder = torch.nn.TransformerEncoder(
+            encoder_layer, num_layers, layer_norm, enable_nested_tensor
+        )
+
+    @property
+    def downsampling_factor(self) -> int:
+        return 1
+
+    def get_mask(self, x: torch.Tensor, lens: Optional[torch.Tensor]) -> torch.Tensor:
+        N, T = x.shape[:2]
+        out_shape = (self.num_heads * N, T, T)
+        if lens is None:
+            len_mask = x.new_zeros(out_shape)
+        else:
+            len_mask = x.new_full(out_shape, -float("inf"))
+            len_mask = len_mask.masked_fill_(
+                get_length_mask(len_mask, lens.repeat_interleave(self.num_heads)), 0
+            )
+        return len_mask
+
+    def encode(
+        self, x: torch.Tensor, lens: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        mask = self.get_mask(x, lens)
+        x = self.encoder(x, mask)
+        return x, lens
+
+
+@_parametrize(
+    "Encoder",
+    [IdentityEncoder, ConvEncoder, TransformerEncoder],
+    ids=["IdentityEncoder", "ConvEncoder", "TransformerEncoder"],
+)
+def test_encoder_variable_batches(Encoder: Type[Encoder]):
+    torch.manual_seed(0)
+    N, Tmin, Tmax, H = 10, 300, 600, 256
+    encoder = Encoder(H)
+    encoder.eval()
+    x, lens = [], []
+    x_exp = []
+    for _ in range(N):
+        lens_n = torch.randint(Tmin, Tmax + 1, (1,))
+        x_n = torch.rand(1, lens_n.item(), H)
+        x_exp_n, lens__n = encoder(x_n)
+        assert not (x_exp_n == 0).all()
+        assert lens__n is None
+        x.append(x_n.flatten())
+        lens.append(lens_n)
+        x_exp.append(x_exp_n.squeeze(0))
+    x = torch.nn.utils.rnn.pad_sequence(x, batch_first=True, padding_value=-1.0)
+    x = x.view(N, -1, H)
+    lens = torch.cat(lens)
+    x_act, lens_act = encoder(x, lens)
+    for n, x_exp_n, x_act_n, lens_act_n in zip(range(N), x_exp, x_act, lens_act):
+        assert lens_act_n == x_exp_n.size(0), n
+        assert (x_act_n[lens_act_n:] == 0).all(), n
+        assert torch.allclose(x_exp_n, x_act_n[:lens_act_n], atol=1e-5), n
--- run.sh
+++ run.sh
@@ -1,11 +1,29 @@
 #! /usr/bin/env bash
 
+# Download librispeech and turn into (raw) tensors. train-clean-100 only train
 python prep/librispeech.py data/librispeech download
-python prep/librispeech.py data/librispeech preamble --speakers-are-readers
+python prep/librispeech.py data/librispeech preamble \
+    --speakers-are-readers --exclude-subsets
 python prep/librispeech.py data/librispeech init_word
-python prep/librispeech.py data/librispeech torch_dir wrd raw_100 --compute-up-to 100 --raw
+python prep/librispeech.py data/librispeech torch_dir wrd raw_100 \
+    --compute-up-to 100 --raw
 
-chunk-torch-spect-data-dir \
-    data/librispeech/raw_100/{train_clean_100,train_clean_100_chunked20480} \
-    --window-type=causal \
-    --lobe-size=20479
+# convert 10ms frame alignments to sample alignments and store the results in
+# train_clean_100/ali
+#
+# N.B. (16000 samps / sec) / (100 frames / sec) = 160 samps / frame
+I=10  # number of parallel processes. You can adjust to your system
+for i in $(seq 1 $I); do
+    unzip -cq resources/converted_aligned_phones.zip | \
+        awk -v spf=160 -v i=$i -v I=$I '
+    (NR + i - 2) % I == 0 {
+        printf "lbi-%s", $1;
+        for (n=2; n <= NF; ++n) for (m=0; m < spf; ++m) printf " %s", $n;
+        printf "\n";
+    }' | \
+        write-table-to-torch-dir \
+            -i iv -o long \
+            'ark,t,s,o:-' \
+            data/librispeech/raw_100/train_clean_100/ali &
+done
+wait
