f1ef27765bba505d72243dec23b5e5d3e373d0a4
Sean Robertson <sdrobert@cs.toronto.edu>
2023-03-03 12:22:07 -0500

    cc changes

--- scripts/cc_run.sh
+++ scripts/cc_run.sh
@@ -1,16 +1,27 @@
 #! /usr/bin/env bash
-#SBATCH --wait
-#SBATCH --time=03:00:00
+#SBATCH --time=05:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
-#SBATCH --signal=SIGUSR1@90
+#SBATCH --signal=B:SIGUSR1@90
 #SBATCH --output=exp/slurm_logs/slurm-%j.out
 
 set -e
+export NCCL_DEBUG=INFO
+
+# we don't rely on pytorch-lightning to requeue b/c CC doesn't allow requeuing
+# via scontrol. Following https://docs.alliancecan.ca/wiki/Running_jobs instead
+do_requeue() {
+    echo -e "SIGUSR1 passed. Requeueing job"
+    sbatch scripts/cc_run.sh "${CC_RUN_OLD_ARGS[@]}"
+    echo -e "Exiting"
+    exit 1
+}
 
 source scripts/cc_env.sh
 
 P="${SLURM_TASKS_PER_NODE:-1}"
 W="${SLURM_CPUS_PER_TASK:-4}"
+CC_RUN_OLD_ARGS=( "$@" )
+trap do_requeue SIGUSR1
 
-source ./run.sh "$@" -s -p "$P" -w "$W" -x "--no-progress-bar"
+source ./run.sh "$@" -s -p "$P" -w "$W" # -x "--no-progress-bar"
