bf3000ee7039fd7fe0d075b66c82fb229838fc92
Sean Robertson <sdrobert@cs.toronto.edu>
2023-03-04 19:29:11 -0500

    More compute canada garbage

--- scripts/cc_run.sh
+++ scripts/cc_run.sh
@@ -1,27 +1,25 @@
 #! /usr/bin/env bash
-#SBATCH --time=05:00:00
+#SBATCH --wait
+#SBATCH --time=3:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
-#SBATCH --signal=B:SIGUSR1@90
+#SBATCH --array=1-100%1
 #SBATCH --output=exp/slurm_logs/slurm-%j.out
+
+# we don't rely on pytorch-lightning to requeue b/c CC doesn't allow requeuing
+# via scontrol. Following https://docs.alliancecan.ca/wiki/Running_jobs, we
+# create an array of jobs instead. If we can complete the run.sh call, we
+# cancel all remaining elements in the array.
 
 set -e
 export NCCL_DEBUG=INFO
-
-# we don't rely on pytorch-lightning to requeue b/c CC doesn't allow requeuing
-# via scontrol. Following https://docs.alliancecan.ca/wiki/Running_jobs instead
-do_requeue() {
-    echo -e "SIGUSR1 passed. Requeueing job"
-    sbatch scripts/cc_run.sh "${CC_RUN_OLD_ARGS[@]}"
-    echo -e "Exiting"
-    exit 1
-}
 
 source scripts/cc_env.sh
 
 P="${SLURM_TASKS_PER_NODE:-1}"
 W="${SLURM_CPUS_PER_TASK:-4}"
-CC_RUN_OLD_ARGS=( "$@" )
-trap do_requeue SIGUSR1
 
-source ./run.sh "$@" -s -p "$P" -w "$W" # -x "--no-progress-bar"
+source ./run.sh "$@" -s -p "$P" -w "$W" -x "--no-progress-bar"
+r=$?
+scancel --state=PENDING "${SLURM_ARRAY_JOB_ID}"
+exit $r
