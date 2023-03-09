13968f34414e722ceba7b7354a08276a350e630a
Sean Robertson <sdrobert@cs.toronto.edu>
2023-03-09 15:00:23 -0500

    Vector stuff

--- scripts/vector_run.sh
+++ scripts/vector_run.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
 #SBATCH --wait
-#SBATCH --partition=t4v2,rtx6000
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=4G
 #SBATCH --cpus-per-task=8
@@ -14,4 +13,4 @@ source scripts/vector_env.sh
 P="${SLURM_TASKS_PER_NODE:-1}"
 W="${SLURM_CPUS_PER_TASK:-4}"
 
-source ./run.sh "$@" -s -p "$P" -w "$W" -x "--no-progress-bar"
+./run.sh "$@" -s -p "$P" -w "$W" -x "--no-progress-bar"
