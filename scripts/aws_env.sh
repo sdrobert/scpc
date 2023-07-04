ab86cd6a1db777f72923a83bbe4627307fcbf789
Sean Robertson <sdrobert@cs.toronto.edu>
2023-07-04 17:41:26 -0400

    Try again

--- scripts/aws_env.sh
+++ scripts/aws_env.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env
 
 source activate pytorch
-conda activate pytorch
-conda install tensorboard
-conda install -c coml virtual-dataset zerospeech-benchmarks zerospeech-libriabx2 zerospeech-tde
-conda install -c sdrobert pydrobert-kaldi pydrobert-param
-pip install "git+https://github.com/sdrobert/pydrobert-pytorch.git@scpc"
-pip install '.'
+conda update --name pytorch -f environment.yaml
+# conda activate pytorch
+# conda install tensorboard pytorch-lightning=1.
+# conda install -c coml virtual-dataset zerospeech-benchmarks zerospeech-libriabx2 zerospeech-tde
+# conda install -c sdrobert pydrobert-kaldi pydrobert-param
+# pip install "git+https://github.com/sdrobert/pydrobert-pytorch.git@scpc"
+# pip install '.'
