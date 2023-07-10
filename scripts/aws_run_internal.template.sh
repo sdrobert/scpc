0849f6a18924eeaf6df31a0759a4eb879b70ff4d
Sean Robertson <sdrobert@cs.toronto.edu>
2023-07-10 11:31:31 -0400

    Dumb fix

--- scripts/aws_run_internal.template.sh
+++ scripts/aws_run_internal.template.sh
@@ -137,5 +137,5 @@ mkdir -p exp/tb_logs
 tensorboard --logdir=exp/tb_logs &
 
 echo "Running with args ${RUN_ARGS[*]}"
-<RUN_SH> "${RUN_ARGS[@]}" -x "--no-progress-bar"
+<RUN_SH> "${RUN_ARGS[@]}" -x "--quiet"
 do_cleanup
