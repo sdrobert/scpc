b48e4f451a2760f317075ea200f409afac1e7a49
sdrobert <sdrobert@cs.toronto.edu>
2023-07-12 14:00:56 -0400

    Fixes

--- scripts/baseline_run.sh
+++ scripts/baseline_run.sh
@@ -301,19 +301,24 @@ for x in dev_clean dev_other test_clean test_other; do
 done
 
 for x in dev_clean dev_other test_clean test_other; do
+    echo "Computing final error rates for $x..."
     if [ ! -f "$bld/scores.$x.txt" ]; then
         cp -f "$dlf/ext/$x.ref.trn" "$bld/$x.ref.chr.trn"
         $cmd_p prep/subword2word.py "$bld/$x.ref."{chr,wrd}".trn"
         for y in lm nolm; do
             $cmd_p torch-token-data-dir-to-trn \
+                --num-workers $nwork \
                 "$bld/$x/$y" "$dlf/ext/id2token.txt" "$bld/$x.hyp.chr.$y.trn"
             $cmd_p prep/subword2word.py "$bld/$x.hyp."{chr,wrd}".$y.trn"
         done
         for y in chr wrd; do
             prep/error-rates-from-trn.py --suppress-warning \
-                "$bld/$x.ref.$y.trn" "$bld/$x.ref.$y.{lm,nolm}.trn" \
-                > "$bld/scores.$x.$y.trn"
+                "$bld/$x.ref.$y.trn" "$bld/$x.hyp.$y."{lm,nolm}.trn \
+                > "$bld/scores.$x.$y.txt"
         done
+        cat "$bld/scores.$x."*.txt > "$bld/scores.$x.txt"
         ((only)) && exit 0
     fi
 done
+
+cat "$bld/scores."{dev_clean,dev_other,test_clean,test_other}.txt
